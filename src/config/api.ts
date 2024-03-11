9f8bd5dd9aa8bdc3f4459d6bb6a4386993219a81
Vasiliy Zadoya <cool29@yandex.ru>
2024-03-11 11:36:55 +0500

    fix

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,6 +3,6 @@ export const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',
 };
 
-export const BASE_URL = 'https://swapi.dev/api/';
+export const BASE_URL = 'https://swapi.py4e.com/api/';
 
 export const DEFAULT_PAGE_ELEMENT_COUNT = 10;
